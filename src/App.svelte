c963286b01a41374f18a7e29e209bc4b09e9b114
Daen Rebel <daenrebel035@gmail.com>
2020-08-23 15:37:13 +0200

    Input

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Header from './components/header.svelte';
 	import CronChecker from './components/cronchecker.svelte';
+import Cronchecker from './components/cronchecker.svelte';
 </script>
 
 <main>
@@ -10,12 +11,11 @@
 			Hi
 		</div> -->
 		<div class="box sidebar">Sidebar</div>
-		<div class="box content">Content
-			<br /> More content than we had before so this column is now quite tall.</div>
+		<div class="box content">
+			<Cronchecker />
+		</div>
 		<div class="box footer">Footer</div>
 	</div>
-	<!-- <Header/>
-	<CronChecker/> -->
 </main>
 
 <style global lang="scss">
--- src/components/cronchecker.svelte
+++ src/components/cronchecker.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-
+    let cron: string = '* * * * * *';
 </script>
 
 <main>
-    
+    every day at {cron}<br>
+    <input bind:value={cron}>
 </main>
 
 <style lang="scss">
